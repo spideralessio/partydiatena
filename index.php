d5ed43894afad5334680fd2016fe238f81077562
Alessio Ragno <spideralessio97@gmail.com>
2017-04-20 19:49:32 +0200

    Add Next Events

--- index.php
+++ index.php
@@ -72,9 +72,10 @@
 						$lat = $_GET["lat"];
 						$lng = $_GET["lng"];
 						if ($_GET["lat"]){
+							echo "<h4>Prossimi eventi vicino a ".$_GET["name"].":</h4>";
 							$sql_events = "SELECT *, (((acos(sin((".$lat."*pi()/180))*sin((Event.lat*pi()/180))+cos((".$lat."*pi()/180))*cos((Event.lat*pi()/180))*cos(((".$lng."-Event.lng)*pi()/180))))*180/pi())*60*1.1515*1.609344) AS dist FROM Event HAVING dist <= 100 AND Event.date >= CURDATE() ORDER BY Event.date;";
 						}else{
-							
+							echo "<h4>Prossimi eventi:</h4>";
 							$sql_events = "SELECT * FROM Event ORDER BY Event.date;";
 						}
 						//echo $sql_events;
@@ -87,7 +88,7 @@
 							array_push($events_arr, $event);
 						  }
 						  foreach ($events_arr as $event){
-						  	echo "<h4>Prossimi Eventi:</h4>";
+
 							echo '
 							<div class="card">
 								<div class="card-image">
